5fac0d1411ed54cb2a16e8a40e97c249bd8172c7
Slavic <hszuesz@gmail.com>
2011-12-28 09:19:01 +0000

    Added functions.js with collection of usefull functions for everyday use

--- src/js/jTooltipp.js
+++ src/js/jTooltipp.js
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2009 Holger Szuesz, <hszuesz@gmail.com>
+ * Copyright (c) 2010 Holger Szuesz, <hszuesz@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
